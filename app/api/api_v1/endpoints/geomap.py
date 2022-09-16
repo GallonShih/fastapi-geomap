a762af9fe8eade999bd4e33ae5d4910b6d63b2ce
GallonShih <gallonshih0526@gmail.com>
2022-09-17 00:54:38 +0800

    refactor: docker-compose init

--- app/api/api_v1/endpoints/geomap.py
+++ app/api/api_v1/endpoints/geomap.py
@@ -10,16 +10,12 @@ History:
 2022/09/12 Created by Gallon
 """
 
-from fastapi import APIRouter, Body, Depends, status, Request
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-# from crud import crud_features_library
 from core.core_config import settings
-# from core.db_session import get_db_session
 from logging import config as logger_config
 from logging import getLogger
-# from sqlalchemy.orm import Session
 
 logger_config.dictConfig(settings.LOGGER_CONF)
 logger = getLogger(__name__)
@@ -31,3 +27,4 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/", response_class=HTMLResponse)
 async def get_map(request: Request):
     return templates.TemplateResponse("map.html", {"request": request})
+    # return 'success'
--- dockerfile
+++ dockerfile
@@ -3,6 +3,6 @@ FROM tiangolo/uvicorn-gunicorn:python3.9-slim
 LABEL maintainers="Gallon"
 
 RUN apt-get -qq update && apt-get -qq -y install build-essential python3-dev libpq-dev git
-COPY . /app
+COPY ./app/requirements.txt /app
 WORKDIR /app
-RUN pip install --no-cache-dir -r /app/api_requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
